1288de61a722e3f867696da306f892047681b6b9
SimpleGeek <isaiah@livebythecode.us>
2021-03-17 18:44:25 -0400

    Fixed list ID for whitelist endpoint

--- src/components/Whitelist.svelte
+++ src/components/Whitelist.svelte
@@ -45,7 +45,7 @@
 
     let domainName = null;
     let domains = null;
-    const listId = 1;
+    const listId = 0;
     const endpoint = 'squid-configuration/whitelist-domains/' + listId;
 
     function clearInput() {
